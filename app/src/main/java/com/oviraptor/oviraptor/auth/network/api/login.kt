4d56845cb7570e2d01516a216db9d7770687212c
iszero12 <binj1246@gmail.com>
2024-10-29 18:17:20 +0900

    feat :: add login

---
+++ app/src/main/java/com/oviraptor/oviraptor/auth/network/api/login.kt
@@ -0,0 +1,25 @@
+package com.oviraptor.oviraptor.auth.network.api
+
+import android.content.Context
+import com.iszero.dgsw_chatting.network.auth.LoginRequest
+import com.oviraptor.oviraptor.auth.network.Client
+import com.oviraptor.oviraptor.auth.userinfo.saveAccToken
+import com.oviraptor.oviraptor.auth.userinfo.saveRefToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+suspend fun login(context: Context, email: String, password: String): String? {
+    return withContext(Dispatchers.IO) {
+        try {
+            val authService = Client.authService
+            val request = LoginRequest(email, password)
+            val response = authService.login(request)
+            saveAccToken(context, response.data.accessToken)
+            saveRefToken(context, response.data.refreshToken)
+            "success"
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
