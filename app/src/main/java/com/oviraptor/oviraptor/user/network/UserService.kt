de8d1ba879cb63d353f03c7ea527fb145a799abd
iszero12 <binj1246@gmail.com>
2024-10-29 19:36:58 +0900

    rename :: from login to user

--- app/src/main/java/com/oviraptor/oviraptor/user/network/UserService.kt
+++ app/src/main/java/com/oviraptor/oviraptor/user/network/UserService.kt
@@ -2,13 +2,13 @@ package com.oviraptor.oviraptor.user.network
 
 import com.iszero.dgsw_chatting.network.auth.RegisterRequest
 import com.iszero.dgsw_chatting.network.auth.LoginRequest
-import com.iszero.dgsw_chatting.network.auth.LoginResponse
+import com.iszero.dgsw_chatting.network.auth.UserResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface UserService {
     @POST("user/login")
-    suspend fun login(@Body request: LoginRequest): LoginResponse
+    suspend fun login(@Body request: LoginRequest): UserResponse
 
     @POST("user/register")
     suspend fun register(@Body request: RegisterRequest)
