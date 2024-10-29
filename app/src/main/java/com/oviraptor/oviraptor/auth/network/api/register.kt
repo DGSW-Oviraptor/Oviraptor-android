ac4855e8804a11b956f22c3933e4f75ab7fbf798
iszero12 <binj1246@gmail.com>
2024-10-29 18:17:27 +0900

    feat :: add register

---
+++ app/src/main/java/com/oviraptor/oviraptor/auth/network/api/register.kt
@@ -0,0 +1,20 @@
+package com.oviraptor.oviraptor.auth.network.api
+
+import com.iszero.dgsw_chatting.network.auth.RegisterRequest
+import com.oviraptor.oviraptor.auth.network.Client
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+suspend fun register(email : String, name : String, password : String): String? {
+    return withContext(Dispatchers.IO) {
+        try {
+            val authService = Client.authService
+            val request = RegisterRequest(email, name, password)
+            val response = authService.register(request)
+            "success"
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
