d0ef48d589faea7a83b910714ce15a65655a40d5
iszero12 <binj1246@gmail.com>
2024-11-05 17:15:30 +0900

    fix :: Client

--- app/src/main/java/com/oviraptor/oviraptor/remote/Client.kt
+++ app/src/main/java/com/oviraptor/oviraptor/remote/Client.kt
@@ -2,23 +2,26 @@ package com.oviraptor.oviraptor.remote
 
 import com.oviraptor.oviraptor.BuildConfig
 import com.oviraptor.oviraptor.main.network.ChatService
+import com.oviraptor.oviraptor.user.network.UserService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Client {
     private var retrofit: Retrofit? = null
-    fun getClient(baseUrl: String): Retrofit {
+    fun getClient(): Retrofit {
         if (retrofit == null) {
+            val url = BuildConfig.BASE_URL
             retrofit = Retrofit.Builder()
-                .baseUrl(baseUrl)
+                .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
         return retrofit!!
     }
-
     val chatService: ChatService by lazy {
-        val url = BuildConfig.BASE_URL
-        getClient(url).create(ChatService::class.java)
+        getClient().create(ChatService::class.java)
+    }
+    val userService: UserService by lazy {
+        getClient().create(UserService::class.java)
     }
 }
