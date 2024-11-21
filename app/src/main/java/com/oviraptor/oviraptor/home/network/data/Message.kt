7cc196638437327c691070ecad581c3ff32eb659
iszero12 <binj1246@gmail.com>
2024-11-21 18:11:34 +0900

    fix :: edit Message data

--- app/src/main/java/com/oviraptor/oviraptor/home/network/data/Message.kt
+++ app/src/main/java/com/oviraptor/oviraptor/home/network/data/Message.kt
@@ -1,6 +1,9 @@
 package com.oviraptor.oviraptor.home.network.data
 
 data class Message (
+    val chatId : String,
+    val room : String,
     val writer : String,
-    val message : String
+    val content : String,
+    val isMine : String
 )
