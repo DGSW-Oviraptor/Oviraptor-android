b446461a9dba14af2c679b615987df5e5668e9c4
iszero12 <binj1246@gmail.com>
2024-11-21 18:21:35 +0900

    feat :: add getRoomDetail

---
+++ app/src/main/java/com/oviraptor/oviraptor/home/network/api/GetRoomDetailResponse.kt
@@ -0,0 +1,28 @@
+package com.oviraptor.oviraptor.home.network.api
+
+import android.content.Context
+import com.oviraptor.oviraptor.home.network.data.Room
+import com.oviraptor.oviraptor.remote.Client
+import com.oviraptor.oviraptor.user.userinfo.getAccToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+suspend fun getRoomDetail(context: Context, roomId : Int): Room? {
+    return withContext(Dispatchers.IO) {
+        try {
+            val accessToken = getAccToken(context)
+            val chatService = Client.chatService
+            if (accessToken != null){
+                val response = chatService.getRoomDetail(accessToken,roomId)
+                response.data
+            }
+            else{
+                null
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
