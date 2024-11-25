e7bdb6b9cb3acf76c31e84b38618267875a7c12b
iszero12 <binj1246@gmail.com>
2024-11-25 21:53:26 +0900

    feat :: add BackButton

---
+++ app/src/main/java/com/oviraptor/oviraptor/ui/component/BackButton.kt
@@ -0,0 +1,26 @@
+package com.oviraptor.oviraptor.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.oviraptor.oviraptor.R
+
+@Composable
+fun BackButton(
+    modifier: Modifier,
+    navController: NavController
+){
+    Box(
+        modifier = modifier
+            .clickable { navController.popBackStack() }
+    ){
+        Image(
+            painter = painterResource(R.drawable.back_button),
+            contentDescription = "",
+        )
+    }
+}
