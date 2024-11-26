fe519f9389b3743b9f831c682d0259e557b817fe
iszero12 <binj1246@gmail.com>
2024-11-26 11:17:49 +0900

    fix :: remove unused

--- app/src/main/java/com/oviraptor/oviraptor/MainActivity.kt
+++ app/src/main/java/com/oviraptor/oviraptor/MainActivity.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.oviraptor.oviraptor.nav.NavGraph
-import com.oviraptor.oviraptor.ui.theme.OviraptorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +15,5 @@ class MainActivity : ComponentActivity() {
             val navHostController = rememberNavController()
             NavGraph(navController = navHostController)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OviraptorTheme {
-        Greeting("Android")
     }
 }
