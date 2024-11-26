8fec0447139df65f23934adb6132fe30fe7b5e3b
iszero12 <binj1246@gmail.com>
2024-11-26 18:14:33 +0900

    feat :: set Nav

--- app/src/main/java/com/oviraptor/oviraptor/nav/NavGraph.kt
+++ app/src/main/java/com/oviraptor/oviraptor/nav/NavGraph.kt
@@ -1,6 +1,7 @@
 package com.oviraptor.oviraptor.nav
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,12 +10,15 @@ import com.oviraptor.oviraptor.main.ui.view.ChatView
 import com.oviraptor.oviraptor.main.ui.view.HomeView
 import com.oviraptor.oviraptor.user.ui.view.FirstView
 import com.oviraptor.oviraptor.user.ui.view.login.LoginView
-import com.oviraptor.oviraptor.user.ui.view.RegisterView
+import com.oviraptor.oviraptor.user.ui.view.register.RegisterEmailView
+import com.oviraptor.oviraptor.user.ui.view.register.RegisterNameView
+import com.oviraptor.oviraptor.user.ui.view.register.RegisterPasswordView
+import com.oviraptor.oviraptor.user.ui.view.register.RegisterViewModel
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     val startDestination =  NavGroup.FIRST
-
+    val registerViewModel: RegisterViewModel = hiltViewModel()
     NavHost(navController = navController, startDestination = startDestination) {
         composable(route = NavGroup.FIRST) {
             FirstView(navController = navController)
@@ -22,8 +26,14 @@ fun NavGraph(navController: NavHostController) {
         composable(route = NavGroup.LOGIN) {
             LoginView(navController = navController)
         }
-        composable(route = NavGroup.REGISTER) {
-            RegisterView(navController = navController)
+        composable(route = NavGroup.REGISTER_NAME) {
+            RegisterNameView(navController = navController, viewModel = registerViewModel)
+        }
+        composable(route = NavGroup.REGISTER_PASSWORD) {
+            RegisterPasswordView(navController = navController, viewModel = registerViewModel)
+        }
+        composable(route = NavGroup.REGISTER_EMAIL) {
+            RegisterEmailView(navController = navController, viewModel = registerViewModel)
         }
         composable(route = NavGroup.HOME) {
             HomeView(navController = navController)
--- app/src/main/java/com/oviraptor/oviraptor/nav/NavGroup.kt
+++ app/src/main/java/com/oviraptor/oviraptor/nav/NavGroup.kt
@@ -3,7 +3,9 @@ package com.oviraptor.oviraptor.nav
 object NavGroup {
     const val FIRST = "intro"
     const val LOGIN = "login"
-    const val REGISTER = "Register"
+    const val REGISTER_NAME = "register_name"
+    const val REGISTER_EMAIL = "register_email"
+    const val REGISTER_PASSWORD = "register_password"
     const val HOME = "home"
     const val FRIEND = "friend"
     const val CHAT = "chat"
