5b3a894f4463f78129edb00c900e76414aad8764
iszero12 <binj1246@gmail.com>
2024-11-27 19:55:59 +0900

    feat :: Implementation of entering chat room

--- app/src/main/java/com/oviraptor/oviraptor/main/ui/view/home/HomeView.kt
+++ app/src/main/java/com/oviraptor/oviraptor/main/ui/view/home/HomeView.kt
@@ -2,18 +2,33 @@ package com.oviraptor.oviraptor.main.ui.view.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -24,11 +39,18 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.oviraptor.oviraptor.R
+import com.oviraptor.oviraptor.main.network.data.Room
+import com.oviraptor.oviraptor.nav.NavGroup
 import com.oviraptor.oviraptor.ui.theme.MainColor
 import com.oviraptor.oviraptor.ui.theme.pretendard
 
 @Composable
 fun HomeView(navController: NavController, viewModel: HomeViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        viewModel.getRooms(context)
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -58,6 +80,65 @@ fun HomeView(navController: NavController, viewModel: HomeViewModel = viewModel(
                 contentDescription = "",
             )
         }
+        LazyColumn (
+            modifier = Modifier
+                .padding(top = 73.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            items(uiState.rooms){ room->
+                RoomItem(
+                    room,
+                    onClick = {
+                        navController.navigate("${NavGroup.CHAT}/${room.id}")
+                    }
+                )
+            }
+        }
+    }
+}
+@Composable
+fun RoomItem(
+    room : Room,
+    onClick: () -> Unit = {},
+){
+    Row(
+        Modifier
+            .padding(horizontal = 18.dp)
+            .fillMaxWidth()
+            .height(75.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .background(Color(0xFFF9F9F9))
+            .border(width = 1.dp, color = Color(0xFFE0E0E0), shape = RoundedCornerShape(5.dp))
+            .clickable { onClick() }
+    ){
+        Spacer(Modifier.width(17.dp))
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(41.dp),
+            painter = painterResource(R.drawable.chat_image),
+            contentDescription = ""
+        )
+        Spacer(Modifier.width(15.dp))
+        Column(
+            modifier = Modifier.align(Alignment.CenterVertically)
+        ){
+            Text(
+                room.name,
+                fontFamily = pretendard,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+            )
+            Text(
+                "${room.participants.size}명 참가중",
+                fontFamily = pretendard,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color(0XFF878787)
+            )
+        }
     }
 }
 
@@ -66,5 +147,6 @@ fun HomeView(navController: NavController, viewModel: HomeViewModel = viewModel(
 )
 @Composable
 fun HomeViewPRV(){
-    HomeView(navController = NavController(context = LocalContext.current))
+//    HomeView(navController = NavController(context = LocalContext.current))
+    RoomItem(room = Room(0,"테스트", listOf("ㅁㄴㅇㄹ","ㄹㅁㅇ")))
 }
